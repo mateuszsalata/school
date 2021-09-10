518b4e879d272bc1c644b83afd6239b609639a05
mateuszsalata <90031917+mateuszsalata@users.noreply.github.com>
2021-09-10 11:19:29 +0200

    Add files via upload

--- zsk/programowanieObiektowe/project1/main.cpp
+++ zsk/programowanieObiektowe/project1/main.cpp
@@ -5,29 +5,64 @@ using namespace std;
 class Worker{
 
     public:
-//Deklaracja zmiennych cz³onkowskich
-        string name;
-        string surname;
-        int age; //dokoñczyæ, sprawdziæ typ danych
+        string name, surname, personality;
+        unsigned short int yearBirthday;
+		float height;
+		char gender; 
 
-//Definicje funkcji cz³onkowskich (metody)
         void showSurname()
         {
             cout << "Nazwisko pracownika: " << surname;
         };
-
-        void Worker::showAllData(){
-        	echo << "Imie i nazwisko: " << name << " " << surname;
-		}
+        
+        void showPersonality();
+        void showName();
+        void showAllData();
 };
+
+    void Worker::showPersonality()
+	{
+    	cout << "Imie i nazwisko: " << name << " " << surname << endl;
+	}
+	
+	void Worker::showName()
+	{
+		cout << "/nImie pracownika: " << name;
+	}
+	
+	void Worker::showAllData()
+	{
+		showPersonality();
+		cout << "Narodowosc: " << personality << endl <<
+		"Rok urodzenia: " << yearBirthday << endl <<
+		"Wzrost: " << height << "cm" << endl <<
+		"Plec: ";
+		
+		switch(gender)
+		{
+		case 'm':
+			cout << "mezczyzna";
+			break;
+		case 'w':
+			cout << "kobieta";
+			break;
+		default:
+			cout << " - ";
+			break;
+		}
+	};
 
 int main(int argc, char** argv)
 {
-	setlocale(LC_TYPE, "polish")
+	//setlocale(LC_TYPE, "polish")
 	
     Worker worker1;
     worker1.name = "Krystian";
     worker1.surname = "Nowak";
+    worker1.yearBirthday = 2006;
+    worker1.personality = "Polish";
+    worker1.height = 175.1;
+    worker1.gender = 'm';
 
     cout << worker1.name;
 	worker1.showSurname();
